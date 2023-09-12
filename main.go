fec9a1d0064355ec8d9de5e5db53b209f2535db0
huyvan <huyvan@kobiton.com>
2023-09-12 11:34:37 +0700

    add new log
    add new log

--- main.go
+++ main.go
@@ -97,6 +97,7 @@ func main() {
 
 	if reportUrl != "" {
 		log.Println("Report URL: ", reportUrl)
+		log.Println("Execute session with create session and generate test run is successful")
 	}
 	//
 	// --- Step Outputs: Export Environment Variables for other Steps:
